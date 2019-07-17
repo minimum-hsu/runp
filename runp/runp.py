2c681617404b07c8903f2650be33bcd91869fdc3
Jung-Lun Hsu <minimum@Minimum-Hsu.local>
2019-07-17 11:27:53 +0800

    support python3

--- runp/runp.py
+++ runp/runp.py
@@ -81,7 +81,7 @@ def get_function(functions, function_name):
 
 
 def print_functions(functions):
-    print "Available functions:"
+    print("Available functions:")
     for fname, function in functions.iteritems():
         doc = get_docstring(function, abbrv=True)
         print(fname + "\t" + doc)
